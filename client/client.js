849152992a5da4a503b1b3e9dd7790634c3ee223
Sebastian Ilves <sebastianilves@Sebastians-MacBook.local>
2014-01-14 23:15:53 +0100

    Colors for spk box

--- client/client.js
+++ client/client.js
@@ -2,14 +2,27 @@
 Session.set("mangd", 1);
 Session.set("halt", 0.01);
 Session.set("pris", 1);
+Session.set("apk", 0);
 Session.set("name", "a");
 Session.set("creator", "a");
+Session.set("location", "");
 
-Template.page.apk = function() {
-  var apk = Math.round(100 * (Session.get("mangd") * Session.get("halt")) / Session.get("pris")) / 100;
-  Session.set('apk', apk);
-  return apk;
-};
+Template.apkbox.preserve(['.box']);
+
+Template.apkbox.helpers({
+	apk: function() {
+  		var apk = Math.round(100 * (Session.get("mangd") * Session.get("halt")) / Session.get("pris")) / 100;
+  		Session.set('apk', apk);
+  		return apk;
+	},
+	state: function() {
+		if (Session.get('apk') < 1) {
+			return "low";
+		} else {
+			return "high";
+		}
+	}
+});
 
 Template.page.events({
   'click input': function() {
@@ -20,8 +33,6 @@ Template.page.events({
   },
   'keyup input.mangd': function(e, t) {
     Session.set("mangd", t.find(".mangd").value);
-    
-    Session.set('location', 'hoho');
   },
   'keyup input.halt': function(e, t) {
     Session.set("halt", t.find(".halt").value / 100);
@@ -60,10 +71,20 @@ Template.list.helpers({
 
 Template.list.events({
 	'click button.save': function(e, t) {
-		var drink = t.find(".name").value;
-		var creator = t.find(".creator").value;
+		Session.set("name", t.find(".name").value);
+		Session.set("creator", t.find(".creator").value);
 		var type = t.find(".type").value;
-      	console.log("Test: "+drink+" "+creator+" "+type+" "+Session.get("apk"));
+      	console.log("Test: "+name+" "+creator+" "+type+" "+Session.get("apk"));
+      	if(name!="" && creator!="") {
+			Calculations.insert({
+				name: Session.get("name"), 
+				creator: Session.get("creator"), 
+				type: type, 
+				location: Session.get("location"),
+				apk: Session.get("apk"), 
+				datetime: (new Date()).getTime()
+			});
+      	}
 	},
   'click button.location': function() {
     if (navigator.geolocation) {
